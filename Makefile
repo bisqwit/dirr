f3b3f315e672a87092d8967cae1602c228d28e62
Joel Yliluoma <joel.yliluoma@iki.fi>
2000-08-20 21:40:46 +0300

    Release version 3.15

--- Makefile
+++ Makefile
@@ -1,14 +1,15 @@
-VERSION = 3.14
+VERSION = 3.15
 
-# Explanations:
+# Obligated defines:
 #   CACHE_GETSET     Recommended, adds speed
 #   CACHE_NAMECOLOR  This too
 #   CACHE_UIDGID     This too
-#   NEW_WILDMATCH    This too, disable if you have problems with it
-#   SUPPORT_BRACKETS They can be disabled
-#   STARTUP_COUNTER  ...if you want a progress meter
+#   NEW_WILDMATCH    Recommended, disable if you have problems with it
+#   SUPPORT_BRACKETS They can be disabled, but I don't see why.
+#   STARTUP_COUNTER  Enable, if you want a progress meter
 #   PRELOAD_UIDGID   Set this to 1 if your passwd file is quick to load
 #   HAVE_STATFS      Disable it if you have no statfs() function.
+#   SETTINGSFILE     File containing the settings. Default: dirrsets.h
 
 DEFINES = -DCACHE_GETSET=1 \
           -DCACHE_NAMECOLOR=1 \
@@ -17,23 +18,23 @@ DEFINES = -DCACHE_GETSET=1 \
           -DSUPPORT_BRACKETS=1 \
           -DSTARTUP_COUNTER=0 \
           -DPRELOAD_UIDGID=0 \
-          -DHAVE_STATFS=1
+          -DHAVE_STATFS=1 \
+          -DSETTINGSFILE=\"dirrsets.h\"
 
 GXX=g++
 CPPFLAGS=-Wall -W -pedantic -DVERSION=\"$(VERSION)\" $(DEFINES)
-CXXFLAGS=-O -g
-LDFLAGS=-g
+CXXFLAGS=-O3 -fomit-frame-pointer
+LDFLAGS=-s
 
 BINDIR=/usr/local/bin
 
 ARCHNAME=dirr-$(VERSION)
-ARCHFILES=dirr.cc oldversions COPYING ChangeLog
+ARCHFILES=dirr.cc oldversions COPYING ChangeLog README dirrsets.h
 
 dirr: dirr.o
 	$(CXX) $(LDFLAGS) -o $@ $^
 
 install: dirr
 	cp -p dirr ${BINDIR}/
-	strip ${BINDIR}/dirr
 
 include depfun.mak
---
+++ dirrsets.h
@@ -0,0 +1,115 @@
+    /**************************************************************
+	 * mode() - How to color the drwxr-xr-x string.
+	 *
+	 * ?               is the unknown case
+	 * #               is for numeric modes
+     " S,H,R,A         are for dos attributes
+	 * l,d,x,c,b,p,s,- are the corresponding characters
+	 *                 in the mode string respectively
+     **************************************************************/
+
+    #ifdef DJGPP
+    "mode(SB,H8,R9,A3,-7,#3)"
+    #else
+    "mode(lB,d9,x9,cE,bE,p6,sD,-7,?C,#3)"
+    #endif
+    
+    /**************************************************************
+     * info() - How to color the type characters
+     *          in the end of filenames.
+     *          
+     * / is for directories
+     * * for executables
+     * @ for links
+     * = for sockets
+     * | for pipes
+     * ? for links which of destinations does not exist
+     *
+     * Note: Colour 0 disables the character being printed.
+     *
+     **************************************************************/
+    "info(/1,*2,@3,=5,|8,?C)"
+    
+    /**************************************************************
+     * owner() - How to color the file owner name.
+     *
+     * First is the color in the case of own file,
+     * second for the case of file not owned by self.
+     **************************************************************/
+    "owner(4,8)"
+    
+    /**************************************************************
+     * type() - How to color the file names, if it does
+     *          not belong to any class of byext()'s.
+     *
+     * l=links, d=directories, x=executable
+     * c=character devices, s=sockets
+     * b=block devices, p=pipes.
+     * ? is for file/link names which were not stat()able.
+     **************************************************************/
+    "type(lB,d9,xA,cE,bE,p6,sD,?3)"
+    
+    /**************************************************************
+     * descr() - How to color the <DIR>, <PIPE> etc texts
+     **************************************************************/
+    "descr(3)"
+    
+    /**************************************************************
+     * date() - How to color the datetimes
+     **************************************************************/
+    "date(7)"
+    
+    /**************************************************************
+     * size() - How to color the numeric file sizes
+     **************************************************************/
+    "size(7)"
+    
+#ifndef DJGPP
+    /**************************************************************
+	 * nrlink() - How to color the number of hard links
+     **************************************************************/
+    "nrlink(7)"
+#endif
+	
+    /**************************************************************
+	 * num() - How to color the numbers in total-sums
+     **************************************************************/
+    "num(3)"
+    
+#ifndef DJGPP
+    /**************************************************************
+     * group() - How to color the file owner name.
+     *
+     * First is the color in the case of file of own group,
+     * second for the case of file of not belonging the group to.
+     **************************************************************/
+    "group(4,8)"
+#endif
+
+    /**************************************************************
+	 * txt() - The default color used for all other texts
+     **************************************************************/
+    "txt(7)"
+    
+    /**************************************************************
+     * byext() - How to color the filenames that match one or more
+     *           of the patterns in the list
+     *
+     * The first argument is the color code. It may have 'i' at end
+     * of it. If it has 'i', the file name patterns are case insensitive.
+     * 
+     * There may be multiple byext() definitions.
+     *
+     * See the documentation at function WildMatch in this source
+     * code for more information about the wildcards.
+     **************************************************************/
+     
+    "byext(2i *.so *.o *.vxd *.dll *.drv *.obj *.dll *.a)"
+    "byext(3i *.txt *.htm *.html *.1st *.wri *.ps *.doc readme quickstart)"
+    "byext(4i core DEADJOE)"
+    "byext(5i *.m?d *.mtm *.s3m *.xm* *.mp\\d *.wav *.smp *.au *.ult *.669)"
+    "byext(6i *.?as *.c *.cpp *.cc *.asm *.s *.inc *.h *.hh *.pov *.irc *.hpp *.src *.ttt *.pp *.p)"
+    "byext(8i *~ *.bak *.old *.bkp *.st3 *.tmp *.$$$ tmp* *.out *.~*)"
+    "byext(Ai *.exe *.com *.bat)"
+    "byext(Ci *.tar *.*z *.bz2 *.zip *.arj *.lzh *.lha *.rar *.deb *.rpm *.a\\d\\d)"
+    "byext(Di *.gif *.bmp *.mp* *.x?m *.mov *.tga *.png *.tif *.pcx *.lbm *.img *.jpg *.jpeg *.fl\\w)"
